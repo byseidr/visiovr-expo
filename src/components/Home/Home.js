20fee029e1a812ecdbe775954c7415b29e9dd8e5
Seidr <seidr@byseidr.com>
2024-09-07 10:48:50 -0300

    Add initial VisioVR info

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,49 +1,61 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/home-main.png";
+import textLogo from "../../Assets/text-logo.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
-  return (
-    <section>
-      <Container fluid className="home-section" id="home">
-        <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+    return (
+        <section>
+            <Particle />
+            <Container fluid className="home-section" id="home">
+                <Container className="home-content">
+                    <Row>
+                        <Col md={7} className="home-header">
+                            <h1
+                                style={{ paddingBottom: 15 }}
+                                className="heading"
+                            >
+                                Olá!{" "}
+                                <span
+                                    className="wave"
+                                    role="img"
+                                    aria-labelledby="wave"
+                                >
+                                    👋🏻
+                                </span>
+                            </h1>
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
-              </h1>
+                            <h1 className="heading-name">
+                                Nós somos a
+                                <img
+                                    className="text-logo"
+                                    alt="VisioVR"
+                                    src={textLogo}
+                                />
+                            </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
+                            <div style={{ padding: 50, textAlign: "left" }}>
+                                <Type />
+                            </div>
+                        </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </Container>
-      <Home2 />
-    </section>
-  );
+                        <Col md={5} style={{ paddingBottom: 20 }}>
+                            <img
+                                src={homeLogo}
+                                alt="home pic"
+                                className="img-fluid"
+                                style={{ maxHeight: "450px" }}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+            <Home2 />
+        </section>
+    );
 }
 
 export default Home;
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,21 +2,22 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+    return (
+        <Typewriter
+            options={{
+                strings: [
+                    "Realidade Virtual",
+                    "Realidade Aumentada",
+                    "Aplicações Educacionais",
+                    "Aplicações de Saúde",
+                    "Aplicações Empresariais",
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+            }}
+        />
+    );
 }
 
 export default Type;
